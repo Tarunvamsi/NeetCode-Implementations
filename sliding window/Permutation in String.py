0b891b67a28c2d28566bbe56043f9c51ea0d8d9d
PUSARLA TARUN VAMSI <85088665+Tarunvamsi@users.noreply.github.com>
2023-08-08 23:52:29 +0530

    Update Permutation in String.py

--- sliding window/Permutation in String.py
+++ sliding window/Permutation in String.py
@@ -4,14 +4,14 @@ In other words, return true if one of s1's permutations is the substring of s2.'
 
 
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
+    def checkInclusion(self, s1: str, s2: str) -> bool:           # Calculate the length of the sliding window (s1's length)
         window = len(s1)
         rev = s1[::-1]
         
         if len(s2) < window:
             return False
         
-        s1_count = [0] * 26
+        s1_count = [0] * 26       # Initialize frequency arrays for characters in s1 and the current window of s2
         s2_count = [0] * 26
         
         for i in range(window):
