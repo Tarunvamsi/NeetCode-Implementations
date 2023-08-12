4b9822cdb09eb8c265570ac83ed42ebeb77b7443
PUSARLA TARUN VAMSI <85088665+Tarunvamsi@users.noreply.github.com>
2023-08-12 23:14:00 +0530

    Update Kth Largest Element in an Array.py

--- Heap or priority queue/Kth Largest Element in an Array.py
+++ Heap or priority queue/Kth Largest Element in an Array.py
@@ -8,5 +8,5 @@ Can you solve it without sorting?
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort(reverse=True)
-        return nums[k-1]
+        nums.sort(reverse=True)                       #sorts the list nums in descending order. 
+        return nums[k-1]                              #returns the element at index k-1 from the sorted list.
