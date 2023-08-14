289ac0f6e7023824c56e3d4cd50cf9ce7ed1320f
PUSARLA TARUN VAMSI <85088665+Tarunvamsi@users.noreply.github.com>
2023-08-14 23:48:57 +0530

    Update Longest Substring Without Repeating Characters.py

--- sliding window/Longest Substring Without Repeating Characters.py
+++ sliding window/Longest Substring Without Repeating Characters.py
@@ -11,6 +11,8 @@ Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.'''
 
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         charSet = set()                                       # Set to store characters in the current substring
