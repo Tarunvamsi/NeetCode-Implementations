22dd6122fadbb067a6c85e1c2f99add553f9f576
PUSARLA TARUN VAMSI <85088665+Tarunvamsi@users.noreply.github.com>
2023-08-21 23:49:14 +0530

    Update Reorderlist.py

--- LinkedLists/Reorderlist.py
+++ LinkedLists/Reorderlist.py
@@ -4,7 +4,10 @@ L0 → L1 → … → Ln - 1 → Ln
 Reorder the list to be on the following form:
 
 L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
-You may not modify the values in the list's nodes. Only nodes themselves may be changed.'''
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]'''
 
 class Solution:
     def reorderList(self, head: ListNode) -> None:
