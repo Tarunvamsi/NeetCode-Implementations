658b4ef5c4893ddefca62cd0109308cb7df88546
PUSARLA TARUN VAMSI <85088665+Tarunvamsi@users.noreply.github.com>
2023-08-31 10:00:54 +0530

    Time: 55 ms (96.89%) | Memory: 16.4 MB (25.79%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -5,32 +5,22 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy_head = ListNode()
-        current = dummy_head
-
-        # Initialize a carry for addition
-        carry = 0
-        
-        # Iterate through both linked lists and the carry
+        dumy=ListNode()
+        cur=dumy
+        carry=0
         while l1 or l2 or carry:
-            # Get the values of the current nodes (default to 0 if nodes are None)
-            value1 = l1.val if l1 else 0
-            value2 = l2.val if l2 else 0
+            v1= l1.val if l1 else 0
+            v2= l2.val if l2 else 0
 
-            # Calculate the sum of values and the carry for the next digit
-            total_sum = value1 + value2 + carry
-            carry = total_sum // 10  # Calculate the carry
-            current_digit = total_sum % 10  # Calculate the value for the current digit
+            val=v1+v2+carry
+            carry=val//10
+            val=val%10
+            cur.next=ListNode(val)
 
-            # Create a new node for the current digit and append it to the result
-            current.next = ListNode(current_digit)
+            cur=cur.next 
+            l1=l1.next if l1 else None
+            l2= l2.next if l2 else None
+        return dumy.next    
 
-            # Update pointers
-            current = current.next
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
 
-        # Return the result, excluding the dummy head
-        return dummy_head.next
-            
-        
+       
