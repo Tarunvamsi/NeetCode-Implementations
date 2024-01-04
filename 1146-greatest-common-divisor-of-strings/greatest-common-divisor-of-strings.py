6a4c37fcc2f48fedd88a017bb19e9781f2efa3ef
PUSARLA TARUN VAMSI <cb.en.u4cse20450@cb.students.amrita.edu>
2024-01-04 15:16:49 +0530

    Time: 15 ms (70.32%) | Memory: 13.3 MB (89.38%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -1,27 +1,9 @@
 class Solution(object):
     def gcdOfStrings(self, str1, str2):
-        s1 = max(str1, str2, key = lambda x: len(x))
-        if s1 == str1:
-            s2 = str2
-        else:
-            s2 = str1
-        a1, a2 = len(s1), len(s2)
-
-        if s2 * (a1//a2) == s1:
-            return s2
-        
-        for i in range(2, a2//2 + 1):
-            if (a2 / i) % 1 == 0:
-                if s2[:a2//i]*i == s2:
-                    if s1[:a2//i]*int(i*a1/a2) == s1:
-                        if s2[:a2//i] == s1[:a2//i]:
-                            return s2[:a2//i] 
-
-        if s1.count(s1[0]) == a1:
-            if s2.count(s2[0]) == a2:
-                if s1[0] == s2[0]:
-                    return s1[0]
-
-        return ''
-           
-        
+        if(len(str2)>len(str1)):
+            str2, str1= str1, str2
+        if str2 == str1:
+            return str1
+        if str1[:len(str2)] != str2:
+            return ""
+        return self.gcdOfStrings(str1[len(str2):],str2)            
