154cf82e61a9c038c9cde8d2d9a464539c3ce611
PUSARLA TARUN VAMSI <cb.en.u4cse20450@cb.students.amrita.edu>
2024-01-04 15:47:21 +0530

    Time: 18 ms (77.21%) | Memory: 13.3 MB (62.21%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -2,3 +2,15 @@ class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
         max_candies = max(candies)
         return [candy + extraCandies >= max_candies for candy in candies]
+
+
+# class Solution(object):
+#     def kidsWithCandies(self, candies, extraCandies):
+#         res=[]
+#         maxc= max(candies)
+#         for i in range(len(candies)):
+#             if (candies[i]+extraCandies >= maxc):
+#                 res.append(True)
+#             else:
+#                 res.append(False)    
+#         return res        
