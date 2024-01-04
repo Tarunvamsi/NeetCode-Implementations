6d4b53ad0e4c3db43a254c2765b01ab8100ebc87
PUSARLA TARUN VAMSI <cb.en.u4cse20450@cb.students.amrita.edu>
2024-01-04 15:46:19 +0530

    Time: 16 ms (84.01%) | Memory: 13.3 MB (62.21%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,10 +1,4 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
-        res=[]
-        maxc= max(candies)
-        for i in range(len(candies)):
-            if (candies[i]+extraCandies >= maxc):
-                res.append(True)
-            else:
-                res.append(False)    
-        return res        
+        max_candies = max(candies)
+        return [candy + extraCandies >= max_candies for candy in candies]
