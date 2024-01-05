3bfd8b236840bae29c1449095e22d6daff47c523
PUSARLA TARUN VAMSI <cb.en.u4cse20450@cb.students.amrita.edu>
2024-01-05 11:10:51 +0530

    Time: 21 ms (36.80%) | Memory: 14 MB (8.65%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,4 +1,24 @@
 class Solution(object):
     def reverseWords(self, s):
-        reversed_string = ' '.join(reversed(s.split()))
+        def reverse_word(word):
+            return word[::-1]
+
+        def reverse_string(string):
+            reversed_str = ""
+            for char in string:
+                reversed_str = char + reversed_str
+            return reversed_str
+
+        words = []
+        start = 0
+        for end in range(len(s)):
+            if s[end] == ' ':
+                if start < end:
+                    words.append(reverse_word(s[start:end]))
+                start = end + 1
+
+        if start < len(s):
+            words.append(reverse_word(s[start:]))
+
+        reversed_string = reverse_string(' '.join(words))
         return reversed_string
