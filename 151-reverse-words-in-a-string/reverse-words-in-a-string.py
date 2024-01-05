790764186ee4fa34d091bf744f861cea84777ead
PUSARLA TARUN VAMSI <cb.en.u4cse20450@cb.students.amrita.edu>
2024-01-05 11:12:24 +0530

    Time: 7 ms (99.02%) | Memory: 13.7 MB (57.30%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,24 +1,33 @@
 class Solution(object):
     def reverseWords(self, s):
-        def reverse_word(word):
-            return word[::-1]
-
-        def reverse_string(string):
-            reversed_str = ""
-            for char in string:
-                reversed_str = char + reversed_str
-            return reversed_str
-
-        words = []
-        start = 0
-        for end in range(len(s)):
-            if s[end] == ' ':
-                if start < end:
-                    words.append(reverse_word(s[start:end]))
-                start = end + 1
-
-        if start < len(s):
-            words.append(reverse_word(s[start:]))
-
-        reversed_string = reverse_string(' '.join(words))
+        words = s.split()
+        reversed_string = ' '.join(reversed(words))
         return reversed_string
+
+# class Solution(object):
+#     def reverseWords(self, s):
+#         def reverse_word(word):
+#             return word[::-1]
+
+#         def reverse_string(string):
+#             reversed_str = ""
+#             for char in string:
+#                 reversed_str = char + reversed_str
+#             return reversed_str
+
+#         words = []
+#         start = 0
+#         for end in range(len(s)):
+#             if s[end] == ' ':
+#                 if start < end:
+#                     words.append(reverse_word(s[start:end]))
+#                 start = end + 1
+
+#         if start < len(s):
+#             words.append(reverse_word(s[start:]))
+
+#         reversed_string = reverse_string(' '.join(words))
+#         return reversed_string
+
+
+
